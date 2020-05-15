831e2fb9d562bd6b03402cd875aa7c39cfcb5e50
yashodeep <yashodeepchikte@gmail.com>
2020-05-16 04:21:37 +0530

    first commit

---
+++ src/hompage.component.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+function HomPage(){
+    return (
+        <div className="hompage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span className="subtitle">Shpo Now</span>
+                    </div>               
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">JACKETS</h1>
+                        <span className="subtitle">Shpo Now</span>
+                    </div>               
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">SNEAKERS</h1>
+                        <span className="subtitle">Shpo Now</span>
+                    </div>               
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Hats</h1>
+                        <span className="subtitle">WOMEN'S</span>
+                    </div>               
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Hats</h1>
+                        <span className="subtitle">MEN'S</span>
+                    </div>               
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomPage;
