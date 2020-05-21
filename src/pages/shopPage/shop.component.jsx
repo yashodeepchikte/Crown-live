613ed5434e0d5224da1fc8941eed9d6d47cb31e0
yashodeep <yashodeepchikte@gmail.com>
2020-05-21 23:58:28 +0530

    added category pages

--- src/pages/shopPage/shop.component.jsx
+++ src/pages/shopPage/shop.component.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 import CollectionOverview from "../../components/collections-overview/collections-overview.component"
 
-const ShopPage = ({collections}) => {
+import {Route} from "react-router-dom"
+import CategoryPage from "../../pages/category/category.component"
+
+const ShopPage = ({match}) => {
     return(
-        <div>
-            <CollectionOverview />
+        <div className="shop-page">
+            <Route  exact path={`${match.path}`} component={CollectionOverview}  /> 
+            <Route path={`${match.path}/:category`} component={CategoryPage} />
         </div>
     )
 }
